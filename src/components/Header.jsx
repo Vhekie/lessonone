bc1b7de745a63fa98baa22ce5796ba7de79e65f8
Ajibade Victoria <vaajibade@gmail.com>
2024-01-03 14:58:55 +0100

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Menu from "./Menu";
+import "./header.css";
+
+function Header() {
+  return (
+    <div className="container">
+      <div className="title">Vickky Retaurant</div>
+      <Menu></Menu>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Menu.jsx
@@ -0,0 +1,12 @@
+function Menu() {
+  return (
+    <ul className="menu">
+      <li className="menu-item"> Home</li>
+      <li className="menu-item">Menu</li>
+      <li className="menu-item">About</li>
+      <li className="menu-item">Contact</li>
+    </ul>
+  );
+}
+
+export default Menu;
